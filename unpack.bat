d7ef86bb249298852db70bf5f9d40957c91c3287
Osman Kovan <44976117+osmankovan123@users.noreply.github.com>
2024-04-04 17:29:04 +0300

    Update unpack.bat

--- unpack.bat
+++ unpack.bat
@@ -1,7 +1,12 @@
 @echo off
+echo Enter adobeTemp folder location:
+set /p adobetempfolder=
+echo Enter AdobeLZMA2 folder location:
+set /p adobelzma2folder=
 echo Watching for changes to C:\adobeTemp ...
+goto :top
 :top
-cd %~dp0
-for /r %%a in ("*") do robocopy "C:\adobeTemp" "D:\AdobeLZMA2" /E /NOOFFLOAD /R:0 | find /v "File(s) copied"
+cd "%adobetempfolder%"
+for /r %%a in ("*") do robocopy "%adobetempfolder%" "%adobelzma2folder%" /E /NOOFFLOAD /R:0 | find /v "File(s) copied"
 timeout /t 0 >nul
 goto :top
