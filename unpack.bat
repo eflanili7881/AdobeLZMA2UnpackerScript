8b8ae95e94360db03b1cfa8d9d47274c96d295fa
Osman Kovan <44976117+osmankovan123@users.noreply.github.com>
2024-04-04 17:37:50 +0300

    Update unpack.bat

--- unpack.bat
+++ unpack.bat
@@ -3,7 +3,10 @@ echo Enter adobeTemp folder location:
 set /p adobetempfolder=
 echo Enter AdobeLZMA2 folder location:
 set /p adobelzma2folder=
-echo Watching for changes to C:\adobeTemp ...
+echo Creating folders if they're not exist ...
+mkdir "%adobetempfolder%"
+mkdir "%adobelzma2folder%"
+echo Watching for changes to %adobetempfolder% ...
 goto :top
 :top
 cd "%adobetempfolder%"
